6711790fb57cf75af7da22079a12a8a9a78251fc
Illia Tkach <tkillia06@gmail.com>
2026-06-05 20:05:06 -0400

    create a top bar calnder

---
+++ src/app/hooks/useCalendar.ts
@@ -0,0 +1,51 @@
+import {
+  format,
+  eachDayOfInterval,
+  addWeeks,
+  subWeeks,
+  startOfWeek,
+  endOfWeek,
+} from "date-fns";
+import { useState, useRef } from "react";
+
+export function useCalendar() {
+  const [referenceDate, setReferenceDate] = useState(new Date());
+  const [selectedDay, setSelectedDay] = useState(new Date());
+  const touchStartX = useRef<number | null>(null);
+
+  const start = startOfWeek(referenceDate, { weekStartsOn: 1 });
+  const end = endOfWeek(referenceDate, { weekStartsOn: 1 });
+  const currentWeekDays = eachDayOfInterval({ start, end });
+
+  const handleNextWeek = () => {
+    setReferenceDate((prevDate) => addWeeks(prevDate, 1));
+  };
+
+  const handlePrevWeek = () => {
+    setReferenceDate((prevDate) => subWeeks(prevDate, 1));
+  };
+
+  const handleTouchStart = (e: React.TouchEvent) => {
+    touchStartX.current = e.touches[0].clientX;
+  };
+
+  const handleTouchEnd = (e: React.TouchEvent) => {
+    if (touchStartX.current === null) return;
+    const delta = e.changedTouches[0].clientX - touchStartX.current;
+    const THRESHOLD = 50;
+    if (delta > THRESHOLD) handlePrevWeek();
+    if (delta < -THRESHOLD) handleNextWeek();
+    touchStartX.current = null;
+  };
+
+  // all state and handlers here
+  return {
+    currentWeekDays,
+    selectedDay,
+    setSelectedDay,
+    handleNextWeek,
+    handlePrevWeek,
+    handleTouchStart,
+    handleTouchEnd,
+  };
+}
---
+++ src/components/sections/Calendar.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { FaArrowRight, FaArrowLeftLong } from "react-icons/fa6";
+import { LuDot } from "react-icons/lu";
+import { useCalendar } from "@/app/hooks/useCalendar";
+import { format, isEqual } from "date-fns";
+
+export default function Calendar() {
+  const {
+    currentWeekDays,
+    selectedDay,
+    setSelectedDay,
+    handleNextWeek,
+    handlePrevWeek,
+    handleTouchStart,
+    handleTouchEnd,
+  } = useCalendar();
+
+  return (
+    <div
+      className="bg-[#1f1f1f] flex flex-col gap-6 items-center p-5"
+      onTouchStart={handleTouchStart}
+      onTouchEnd={handleTouchEnd}
+    >
+      {/* top arrow + the header display selected day */}
+      <div className="flex flex-row gap-5">
+        <button
+          className="inline-flex items-center justify-center text-white rounded-lg"
+          onClick={handlePrevWeek}
+        >
+          <FaArrowLeftLong />
+        </button>
+
+        <h1 className="text-white">{format(selectedDay, "EEE, MMM dd")}</h1>
+
+        <button
+          className="inline-flex items-center justify-center text-white rounded-lg"
+          onClick={handleNextWeek}
+        >
+          <FaArrowRight />
+        </button>
+      </div>
+
+      {/* display a list of days (7 days) */}
+      <div className="flex flex-row gap-5 w-full justify-center overflow-x-auto ">
+        {currentWeekDays.map((day) => {
+          const isSelected = isEqual(day.getDate(), selectedDay.getDate());
+          const today = new Date();
+          const isToday = isEqual(day.getDate(), today.getDate());
+          return (
+            <button
+              key={day.toString()}
+              className={`border rounded-4xl w-10 h-18  border-gray-500 ${isSelected ? "bg-[#4b4b4b]" : ""}`}
+              onClick={() => setSelectedDay(day)}
+            >
+              <h2 className="text-center text-white">{format(day, "EEEEE")}</h2>
+              <time className="text-white" dateTime={format(day, "yyyy-MM-dd")}>
+                {day.getDate()}
+              </time>
+              <div className="flex justify-center">
+                {isToday && <LuDot className="text-white" />}
+              </div>
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
